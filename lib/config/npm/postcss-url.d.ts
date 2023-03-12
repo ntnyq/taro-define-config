da258198e4f4d3b1f23a5adbeb2576e3dbc3ec9b
ntnyq <ntnyq13@gmail.com>
2023-03-13 03:42:15 +0800

    feat: add postcss-url type

--- lib/config/npm/postcss-url.d.ts
+++ lib/config/npm/postcss-url.d.ts
@@ -4,4 +4,102 @@
  * @see https://github.com/postcss/postcss-url
  */
 
-export type PostcssUrlOptions = Record<string, any>
+import type { LiteralUnion } from '../../utility-types'
+
+export type PostCSSUrlUrl = LiteralUnion<'copy' | 'inline' | 'rebase'>
+
+export interface PostCSSUrlUrlAsset {
+  url: string
+  pathname?: string
+  absolutePath?: string
+  relativePath?: string
+  search?: string
+  hash?: string
+}
+
+export interface PostCSSUrlUrlDir {
+  from?: string
+  to?: string
+  file?: string
+}
+
+export type PostCSSUrlTransformFn = (
+  asset?: PostCSSUrlUrlAsset,
+  dir: PostCSSUrlUrlDir,
+) => string
+
+export type PostcssUrlFilterFn = (file?: string) => boolean
+
+export type PostcssUrlHashOptionsMethod = LiteralUnion<'xxhash32' | 'xxhash64'>
+
+export type PostcssUrlHashOptionsMethodFn = (file?: Buffer) => string
+
+export interface PostcssUrlHashOptions {
+  method?: PostcssUrlHashOptionsMethod | PostcssUrlHashOptionsMethodFn
+  shrink?: number
+  append?: boolean
+}
+
+export interface PostcssUrlOptions {
+  /**
+   * @see https://github.com/postcss/postcss-url#url
+   * @default `rebase`
+   */
+  url?: PostCSSUrlUrl | PostCSSUrlTransformFn
+
+  /**
+   * @see https://github.com/postcss/postcss-url#maxsize
+   */
+  maxSize?: number
+
+  /**
+   * @see https://github.com/postcss/postcss-url#ignorefragmentwarning
+   * @default false
+   */
+  ignoreFragmentWarning?: boolean
+
+  /**
+   * @default false
+   */
+  optimizeSvgEncode?: boolean
+
+  /**
+   * @see https://github.com/postcss/postcss-url#filter
+   */
+  filter?: string | RegExp | PostcssUrlFilterFn
+
+  /**
+   * @see https://github.com/postcss/postcss-url#includeurifragment
+   * @default false
+   */
+  includeUriFragment?: boolean
+
+  /**
+   * @see https://github.com/postcss/postcss-url#fallback
+   */
+  fallback?: PostCSSUrlTransformFn
+
+  /**
+   * @see https://github.com/postcss/postcss-url#basepath
+   */
+  basePath?: string | string[]
+
+  /**
+   * @see https://github.com/postcss/postcss-url#assetspath
+   * @default false
+   */
+  assetsPath?: boolean | string
+
+  /**
+   * @see https://github.com/postcss/postcss-url#usehash
+   * @default false
+   */
+  useHash?: boolean
+
+  /**
+   * @see https://github.com/postcss/postcss-url#hashoptions
+   */
+  hashOptions?: PostcssUrlHashOptions
+
+  [key: string]: any
+}
