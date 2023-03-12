84a574dbbd0db899d2735bc3dfbebb702fdf72d2
ntnyq <ntnyq13@gmail.com>
2023-03-13 02:56:25 +0800

    feat: add stylus-loader type

--- lib/config/npm/stylus-loader.d.ts
+++ lib/config/npm/stylus-loader.d.ts
@@ -4,4 +4,78 @@
  * @see https://github.com/webpack-contrib/stylus-loader#options
  */
 
-export type StylusLoaderOptions = Record<string, any>
+export type StylusOptionsDefineItemValue = string | number | boolean
+
+export type StylusOptionsDefineItem = [string, StylusOptionsDefineItemValue, boolean?]
+
+export interface StylusOptionsResolveURL {
+  paths?: string[]
+
+  nocheck?: boolean
+}
+
+/**
+ * @see https://github.com/webpack-contrib/stylus-loader#object
+ */
+export interface StylusLoaderStylusOptions {
+  use?: string[] | Function[]
+
+  include?: string[]
+
+  import?: string[]
+
+  define?: StylusOptionsDefineItem[]
+
+  includeCSS?: boolean
+
+  resolveURL?: boolean | StylusOptionsResolveURL
+
+  lineNumbers?: boolean
+
+  hoistAtrules?: boolean
+
+  compress?: boolean
+
+  paths?: string[]
+}
+
+export type StylusLoaderStylusOptionsFn = (loaderContext: any) => string[] | {
+  paths: string[]
+}
+
+// TODO: replace any with webpack LoaderContext
+export type StylusLoaderAdditionalData = (
+  content: string | Buffer,
+  loaderContext?: any,
+  meta?: any,
+) => string
+
+export interface StylusLoaderOptions {
+  /**
+   * @see https://github.com/webpack-contrib/stylus-loader#stylusOptions
+   * @default {}
+   */
+  stylusOptions?: StylusLoaderStylusOptions | StylusLoaderStylusOptionsFn
+
+  /**
+   * @see https://github.com/webpack-contrib/stylus-loader#sourcemap
+   */
+  sourceMap?: boolean
+
+  /**
+   * @see https://github.com/webpack-contrib/stylus-loader#webpackimporter
+   */
+  webpackImporter?: boolean
+
+  /**
+   * @see https://github.com/webpack-contrib/stylus-loader#additionalData
+   */
+  additionalData?: string | StylusLoaderAdditionalData
+
+  /**
+   * @see https://github.com/webpack-contrib/stylus-loader#implementation
+   */
+  implementation?: string | Function
+
+  [key: string]: any
+}
