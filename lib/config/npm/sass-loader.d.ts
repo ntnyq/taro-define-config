4bbbc9e06409b307219b1e9942b8acfa7d8b5998
ntnyq <ntnyq13@gmail.com>
2023-03-13 03:18:53 +0800

    feat: add sass-loader type

--- lib/config/npm/sass-loader.d.ts
+++ lib/config/npm/sass-loader.d.ts
@@ -4,4 +4,51 @@
  * @see https://github.com/webpack-contrib/sass-loader#options
  */
 
-export type SassLoaderOptions = Record<string, any>
+// TODO: replace any with webpack LoaderContext
+export type SassLoaderAdditionalData = (
+  content: string | Buffer,
+  loaderContext?: any,
+) => string
+
+export type SassLoaderSassOptions = Record<string, any>
+
+export type SassLoaderSassOptionsFn = (
+  content: string | Buffer,
+  loaderContext?: any,
+  meta?: any
+) => SassLoaderSassOptions
+
+export interface SassLoaderOptions {
+  /**
+   * @see https://github.com/webpack-contrib/sass-loader#implementation
+   * @default `sass`
+   */
+  implementation?: string | object
+
+  /**
+   * @see https://github.com/webpack-contrib/sass-loader#sassoptions
+   */
+  sassOptions?: SassLoaderSassOptions | SassLoaderSassOptionsFn
+
+  /**
+   * @see https://github.com/webpack-contrib/sass-loader#sourcemap
+   */
+  sourceMap?: boolean
+
+  /**
+   * @see https://github.com/webpack-contrib/sass-loader#additionaldata
+   */
+  additionalData?: string | SassLoaderAdditionalData
+
+  /**
+   * @see https://github.com/webpack-contrib/sass-loader#webpackimporter
+   * @default true
+   */
+  webpackImporter?: boolean
+
+  /**
+   * @see https://github.com/webpack-contrib/sass-loader#warnruleaswarning
+   * @default false
+   */
+  warnRuleAsWarning?: boolean
+}
