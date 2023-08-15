63f1a6285486cb653df9b42592c4261adb087f86
ntnyq <ntnyq13@gmail.com>
2023-08-15 20:21:09 +0800

    chore: cleanup deps

--- taze.config.ts
+++ taze.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'taze'
 
 export default defineConfig({
-  exclude: ['@types/tapable', '@types/webpack', 'webpack', 'html-webpack-plugin'],
+  exclude: ['@types/tapable', '@types/webpack', 'html-webpack-plugin'],
 })
