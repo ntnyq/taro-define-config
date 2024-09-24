7afde7091783b11c158c895787eed971318d6ac3
ntnyq <ntnyq13@gmail.com>
2024-09-25 02:52:15 +0800

    fix: add minimizer `lightningcss`

--- src/config/minimizers/css.d.ts
+++ src/config/minimizers/css.d.ts
@@ -2,6 +2,6 @@ import type { LiteralUnion } from '../../utility-types'
 import type { ConfigurablePlugin } from '../common'
 import type { CSSNanoOptions } from '../packages'
 
-export type CSSMinimizer = LiteralUnion<'csso' | 'esbuild' | 'parcelCss'>
+export type CSSMinimizer = LiteralUnion<'csso' | 'esbuild' | 'lightningcss'>
 
 export type CSSOMinimizer = ConfigurablePlugin<CSSNanoOptions>
