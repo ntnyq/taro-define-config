cce58a481bf3256941d017d7b74cb7c2c68523ba
ntnyq <ntnyq13@gmail.com>
2024-10-28 03:32:49 +0800

    fix: upgrade `less-loader` types

--- src/config/packages/less-loader.d.ts
+++ src/config/packages/less-loader.d.ts
@@ -2,6 +2,7 @@
  * `less-loader` 配置
  *
  * @see https://github.com/webpack-contrib/less-loader#options
+ * @compatibility 12.2.0
  */
 
 import type { LessOptions } from './less'
@@ -10,11 +11,16 @@ import type { WebpackLoaderContext } from './webpack'
 export interface LessLoaderOptions {
   /**
    * less options in camelCase
+   *
+   * @see https://github.com/webpack-contrib/less-loader#lessoptions
+   * @default { relativeUrls: true }
    */
   lessOptions?: LessOptions | ((loaderConext: WebpackLoaderContext) => LessOptions)
 
   /**
    * prepends/appends Less code to the actual entry file
+   *
+   * @see https://github.com/webpack-contrib/less-loader#additionaldata
    */
   additionalData?:
     | string
@@ -22,20 +28,34 @@ export interface LessLoaderOptions {
 
   /**
    * if generation of source maps
+   *
+   * @see https://github.com/webpack-contrib/less-loader#sourcemap
+   * @default compiler.devtool
    */
   sourceMap?: boolean
 
   /**
    * enables/disables the default webpack importer
    *
+   * @see https://github.com/webpack-contrib/less-loader#webpackimporter
    * @default true
    */
   webpackImporter?: boolean
 
   /**
    * determines which implementation of Less to use
+   *
+   * @see https://github.com/webpack-contrib/less-loader#implementation
    */
   implementation?: string | Record<string, any>
+
+  /**
+   * warnings and errors will be webpack warnings and errors, not just logs
+   *
+   * @see https://github.com/webpack-contrib/less-loader#lesslogaswarnorerr
+   * @default false
+   */
+  lessLogAsWarnOrErr?: boolean
 
   [key: string]: any
 }
